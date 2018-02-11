3a12818e6a6bab0fa4c9b0559877f75219437a51
houkangzhu <houkangzhu@tuniu.com>
2018-02-11 16:53:43 +0800

    整理下代码

--- GetAllUTF8/main.m
+++ GetAllUTF8/main.m
@@ -14,91 +14,12 @@ static const NSInteger PART_CHAR_LENTH = 10240;
 static NSString *__ResultPath = nil;
 void setupFilePath(void) {
     __ResultPath = NSSearchPathForDirectoriesInDomains(NSDesktopDirectory, NSUserDomainMask, YES).firstObject;
-    __ResultPath = [__ResultPath stringByAppendingPathComponent:@"字符大全"];
+    __ResultPath = [__ResultPath stringByAppendingPathComponent:@"GetAllUTF8"];
     NSFileManager *fManager = [NSFileManager defaultManager];
     if ([fManager fileExistsAtPath:__ResultPath]) {
         [fManager removeItemAtPath:__ResultPath error:nil];
     }
     [fManager createDirectoryAtPath:__ResultPath withIntermediateDirectories:YES attributes:nil error:nil];
-}
-
-void getAllUtf8Char(void) {
-    NSMutableString *str = [NSMutableString string];
-    __block NSInteger partCount = 0;
-    void (^write_part_block)(NSString *) = ^(NSString *string) {
-        NSString *partPath = [NSString stringWithFormat:@"%@/part_%ld.txt",__ResultPath, partCount];
-        [string writeToFile:partPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
-        partCount ++;
-    };
-    
-    int64_t i = 0;
-    int64_t char_count = 0;
-    while (i <= 0xFFFFFFFFFFFF) {
-        i ++;
-        Byte strByte[6] = {0};
-        int8_t len = 0;
-        Byte head = 0;
-        if (i <= 0xFF) { // 1B
-            len = 1;
-        }
-        else if (i <= 0xFFFF) { // 2B
-            len = 2;
-            head = 0b11011111;
-        }
-        else if (i <= 0xFFFFFF) { // 3B
-            len = 3;
-            head = 0b11101111;
-        }
-        else if (i <= 0xFFFFFFFF) { // 4B
-            len = 4;
-            head = 0b11110111;
-        }
-        else if (i <= 0xFFFFFFFFFF) { // 5B
-            len = 5;
-            head = 0b11111011;
-        }
-        else {  // 6B
-            len = 6;
-            head = 0b11111101;
-        }
-        if (len == 1) {
-            if (i>>7 != 0) continue;
-            strByte[0] = (Byte)(i&0b01111111);
-        }
-        else {
-            BOOL exist = NO;
-            for (int8_t j=0; j<len; j++) {
-                Byte bc = ((i >> ((len-j-1)*8)) & 0xFF);
-                if (j == 0) {
-                    if((bc >> (8-len)) != (head >> (8-len))) {
-                        exist = YES;
-                        break;
-                    }
-                    strByte[0] = (bc & head);
-                }
-                else {
-                    if (bc >> 6 != 0b10) {
-                        exist = YES;
-                        break;
-                    }
-                    strByte[j] = (bc & 0b10111111);
-                }
-            }
-            if (exist) continue;
-        }
-        NSString *charStr = [[NSString alloc] initWithBytes:strByte
-                                                     length:len
-                                                   encoding:NSUTF8StringEncoding];
-        if (charStr.length == 1 || charStr.length == 2) {
-            [str appendString:charStr];
-            char_count ++;
-            if (char_count % PART_CHAR_LENTH == 0) {
-                write_part_block(str);
-                str = [NSMutableString string];
-            }
-        }
-    }
-    write_part_block(str);
 }
 
 // 获取多位时的最大和最小值
@@ -122,8 +43,8 @@ static uint64 getBorderNumber(uint8 len, BOOL start) {
     free(bytes);
     return result;
 }
-// 高效版
-void getAllUtf8Char2(void) {
+
+void getAllUtf8Char(void) {
     __block NSMutableString *resultString = [NSMutableString string];
     __block NSInteger partCount = 0;
     __block int64_t char_count = 0;
@@ -142,7 +63,6 @@ void getAllUtf8Char2(void) {
             }
         }
     };
-    
     {   // 1位的
         for (uint8 i_1 = 0; i_1 <= 0x7F; i_1 ++) {
             Byte strByte = (Byte)(i_1&0x7F);
@@ -163,7 +83,7 @@ void getAllUtf8Char2(void) {
                         strBytes[0] = (bc & head_byte);
                     }
                     else {
-                        strBytes[j] = (bc & 0xBF);
+                        strBytes[j] = (bc & 0xBF);// 1011 1111
                     }
                 }
                 writeBytesBlock(strBytes, len);
@@ -181,10 +101,11 @@ void getAllUtf8Char2(void) {
     [resultString writeToFile:partPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
 
+// UTF-8 https://baike.baidu.com/item/UTF-8/481798?fr=aladdin
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         setupFilePath();
-        getAllUtf8Char2();
+        getAllUtf8Char();
     }
     return 0;
 }
